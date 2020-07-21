cd1c4cf56e9e5b7e8dc70f76256bd77a7cb120c3
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-21 13:39:44 +0200

    Display price as well

--- frontend/src/Components/Cards.js
+++ frontend/src/Components/Cards.js
@@ -15,6 +15,7 @@ const Cards = props => {
                         <div className="card-body">
                             <h5 className="card-title">{item.name}</h5>
                             <p className="card-text">{item.description}</p>
+                            <h6 className="card-text">Price: {item.price} Huf</h6>
                             <p className="card-text"><small className="text-muted">Seller: {item.userId}</small></p>
                         </div>
                     </div>
