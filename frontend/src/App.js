710ff23162494f1e0808e065fe1aed251d6d5ff9
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-21 10:22:29 +0200

    Add NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <div>Hi there</div>
+        <Router>
+            <NavBar />
+            <div>Hi there</div>
+        </Router>
     </div>
   );
 }
---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+
+const NavBar = () => {
+
+    return (
+        <React.Fragment>
+            <NavBarHeader>
+                <MyLink to={"/"}>Home</MyLink>
+            </NavBarHeader>
+        </React.Fragment>
+    );
+};
+
+const NavBarHeader = styled.header`
+  padding: 1rem;
+  background-color: #2b2b2b;
+  display: flex;
+  width: 99.9vw;
+  flex-direction: row;
+  /* justify-content: flex-begin; */
+  margin: 0;
+`;
+const MyLink = styled(Link)`
+  margin: 0.2rem 1rem;
+  color: #ffffff;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+  &:hover {
+    transition: 350ms;
+    color: #a9a9a9;
+    text-decoration: none;
+  }
+  font-size: 1.5rem;
+`;
+
+export default NavBar;
