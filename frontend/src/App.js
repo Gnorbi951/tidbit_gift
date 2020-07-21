11d00dee0c6b9e88d5fe916cf23f40b4c9cb27b7
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-21 14:10:39 +0200

    Implement productDetail routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import MainPage from "./Components/MainPage";
+import ProductDetail from "./Components/ProductDetail";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Router>
             <NavBar />
             <Route exact path={"/"} component={MainPage} />
+            <Route path={"/product/:id"} component={ProductDetail} />
         </Router>
     </div>
   );
---
+++ frontend/src/Components/ProductDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductDetail = props => {
+
+    return (
+        <React.Fragment>
+            hello
+        </React.Fragment>
+    )
+}
+
+export default ProductDetail;
