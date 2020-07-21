0b67f861852f29acd8149c5945a77f3e5684c529
Gőcze Norbert <gnorbi951@gmail.com>
2020-07-21 10:25:32 +0200

    Add MainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import MainPage from "./Components/MainPage";
 
 function App() {
   return (
     <div className="App">
         <Router>
             <NavBar />
-            <div>Hi there</div>
+            <Route exact path={"/"} component={MainPage} />
         </Router>
     </div>
   );
---
+++ frontend/src/Components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainPage = () => {
+
+    return(
+        <React.Fragment>
+            MainPage
+        </React.Fragment>
+    )
+
+}
+
+export default MainPage;
