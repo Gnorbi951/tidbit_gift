83070bd12d401715b6b45c91ed9db9149a09703b
kalnaipeter <kalnaipeter@gmail.com>
2020-07-23 10:42:11 +0200

    delete done

--- frontend/src/Components/SubComponents/DeleteProduct.js
+++ frontend/src/Components/SubComponents/DeleteProduct.js
@@ -1,11 +1,14 @@
 import React from "react";
 import axios from 'axios';
+import {useHistory} from "react-router-dom";
 
 const DeleteProduct = (props) => {
+    const history = useHistory()
 
     const deleteProduct = () => {
-        axios.delete(`http://localhost:8762/productservice/product/${props.id}/${props.userId}`)
+        axios.delete(`http://localhost:8762/productservice/product/${props.id}`)
             .then((response) => {
+                history.push("/")
                 console.log(response)
             })
     }
