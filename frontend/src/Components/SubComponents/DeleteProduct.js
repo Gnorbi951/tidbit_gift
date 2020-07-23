14d2ae5dad3621d06efd65e7eecd952bddcc60cd
dr. Simon Bálint <balladin0@gmail.com>
2020-07-23 14:39:48 +0200

    add reload page if deleting product

--- frontend/src/Components/SubComponents/DeleteProduct.js
+++ frontend/src/Components/SubComponents/DeleteProduct.js
@@ -1,19 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
+// import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import {useHistory} from "react-router-dom";
+// import {useHistory} from "react-router-dom";
 
 const DeleteProduct = (props) => {
-    const history = useHistory();
+    // const history = useHistory();
 
     const deleteProduct = () => {
         axios.delete(`http://localhost:8762/productservice/product/${props.id}`)
             .then((response) => {
-                // if ((window.location.href).includes("my-products")) {
-                //     history.push("/my-products")
-                //     // window.location.reload();
-                // } else {
-                    history.push("/")
-                // }
+                    window.location.reload();
+                //     history.push("/")
             })
     }
 
